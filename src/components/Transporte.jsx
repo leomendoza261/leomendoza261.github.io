f4e3a592ca82273195d09c892e376727eedeba3e
Leo Mendoza <77596@sistemas.frc.utn.edu.ar>
2023-10-29 10:00:00 -0300

    position

--- src/components/Transporte.jsx
+++ src/components/Transporte.jsx
@@ -44,7 +44,30 @@ const Transporte = () => {
         fetchTransportData();
     }, [route]);
 
-
+    /* actuliza la posicion cada 30 segundos */
+    useEffect(() => {
+        const fetchDataInterval = setInterval(() => {
+            async function fetchTransportData() {
+                try {
+                    const response = await fetch(`https://datosabiertos-transporte-apis.buenosaires.gob.ar:443/colectivos/vehiclePositionsSimple?route_id=${route}&client_id=cb6b18c84b3b484d98018a791577af52&client_secret=3e3DB105Fbf642Bf88d5eeB8783EE1E6`);
+                    if (!response.ok) {
+                        throw new Error('Error en la solicitud a la API');
+                    }
+                    const data = await response.json();
+                    setTransportData(data);
+                    console.log(transportData);
+                } catch (error) {
+                    console.error('Error fetching data:', error);
+                }
+            }
+            fetchTransportData();
+        }, 31000); // 31 segundos en milisegundos
+    
+        // Limpia el intervalo cuando el componente se desmonta
+        return () => {
+            clearInterval(fetchDataInterval);
+        };
+    }, [route]);
 
     if (!transportData) {
         return (
@@ -115,7 +138,6 @@ const Transporte = () => {
        
         return segmentsA.length - segmentsB.length;
       }
-
 
     datosFiltrados.sort(customComparator)
 
